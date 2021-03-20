d4276afe14391136a8c0d9027b472dc24adb1b52
Leonardo Spuldar <spdleonardo@hotmail.com>
2021-03-20 20:54:39 -0300

    1.0

---
+++ node/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database:'nodedb'
+};
+const mysql = require('mysql')
+
+app.get('/', async (req,res) => {
+    const connection = mysql.createConnection(config)
+    const presql = "CREATE TABLE IF NOT EXISTS `nodedb`.`people`(`name` VARCHAR(255) NOT NULL);"
+    connection.query(presql, () => {
+        const sql = `INSERT INTO people(name) values('Joãozinho')`
+        connection.query(sql , () => {
+            const sql = `SELECT * from people;`;
+            connection.query(sql, (error, results, fields) => {
+                let html = '<h1>Full Cycle</h1><ul>';
+                results.forEach(person => {
+                    html += `<li>${person.name}</li>`;
+                });
+                html += "</ul>";
+                connection.end();
+                res.send(html);
+            });
+        })
+    }) 
+})
+
+app.listen(port, ()=> {
+    console.log('Rodando na porta ' + port)
+})
